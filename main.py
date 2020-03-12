acd77a89d159c50282e746829097f5167a0c1b58
Guegan <leandre.guegan@linkt.fr>
2020-03-12 14:53:44 +0100

    ajout gestion erreur fichier

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
+class ParseError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = 'Impossible de parser'
+
+    def __str__(self):
+        return self.message
+
 def getDatas():
+    file=None
     try:
-        file = open("D:\coty.csv", "r")
-        datas = []
-        for line in file:
-            line =line.rstrip('\n\r')
-            fileData = line.split(';')
-            if(fileData[0] != '' and fileData[0].isdigit()):
-                datas.append(fileData)
-            elif(fileData[0] == '' and fileData[1] != ''):
-                datas.append(fileData)
-        return datas
-
+        file = open("coty.csv", "r")
     except:
-        print("Erreur lors de la lecture du fichier")
+        raise FileNotFoundError
 
-datas = getDatas()
-for data in datas: print(data)
+    if file == None : raise FileNotFoundError
+
+    datas = []
+    for line in file:
+        try:
+            line = line.rstrip('\n\r')
+            fileData = line.split(';')
+            if (fileData[0] != '' and fileData[0].isdigit()):
+                datas.append(fileData)
+            elif (fileData[0] == '' and fileData[1] != ''):
+                datas.append(fileData)
+        except:
+            raise ParseError
+
+    return datas
+
+
+try:
+    datas = getDatas()
+    for data in datas: print(data)
+except FileNotFoundError:
+    print("Impossible d'ouvrir le fichier")
+except ParseError:
+    print('Impossible de parser le fichier')
