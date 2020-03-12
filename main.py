26587609d121d05e0f3eb282376d87e4f9b843a2
Shae1507 <37331677+Shae1507@users.noreply.github.com>
2020-03-12 14:33:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-try :
-    file = open("coty.csv", "r")
+def getDatas():
+    try:
+        file = open("D:\coty.csv", "r")
+        datas = []
+        for line in file:
+            line =line.rstrip('\n\r')
+            fileData = line.split(';')
+            if(fileData[0] != '' and fileData[0].isdigit()):
+                datas.append(fileData)
+            elif(fileData[0] == '' and fileData[1] != ''):
+                datas.append(fileData)
+        return datas
 
-    for line in file:
-        line =line.rstrip('\n\r')
-        fileData = line.split(';')
-        print(fileData)
-except:
-    print("Erreur lors de la lecture du fichier")
+    except:
+        print("Erreur lors de la lecture du fichier")
 
+datas = getDatas()
+for data in datas: print(data)
